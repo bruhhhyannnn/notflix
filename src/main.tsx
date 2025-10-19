8db1b83b98cde7461eda5b9f9610d8147e2b911d
Bryan Mangapit <mangapit.bryan@gmail.com>
2025-10-19 22:47:03 +0800

    feat(analytics): Add analytics component with a dynamic link of tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import AnalyticsTracker from "./components/analytics/analyticsTracker.tsx";
 import "./css/index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <AnalyticsTracker />
       <App />
     </BrowserRouter>
   </StrictMode>
