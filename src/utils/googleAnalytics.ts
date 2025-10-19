18a4b2974dd61facb5713f5cfc54c86e4bc9d81d
Bryan Mangapit <mangapit.bryan@gmail.com>
2025-10-19 22:46:33 +0800

    feat(analytics): Add google analytics logic

---
+++ src/utils/googleAnalytics.ts
@@ -0,0 +1,32 @@
+export const GA_MEASUREMENT_ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
+
+declare global {
+  interface Window {
+    dataLayer: Record<string, unknown>[];
+    gtag: (...args: any[]) => void;
+  }
+}
+
+export function initGA() {
+  if (!window.gtag && GA_MEASUREMENT_ID) {
+    const script1 = document.createElement("script");
+    script1.async = true;
+    script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+    document.head.appendChild(script1);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(...args: any[]) {
+      window.dataLayer.push(args);
+    }
+    window.gtag = gtag;
+
+    gtag("js", new Date());
+    gtag("config", GA_MEASUREMENT_ID);
+  }
+}
+
+export function trackPageview(path: string) {
+  if (window.gtag) {
+    window.gtag("config", GA_MEASUREMENT_ID, { page_path: path });
+  }
+}
