6efaba643f499deec197f3fd966962279b586aa9
Bryan Mangapit <mangapit.bryan@gmail.com>
2025-10-21 10:23:29 +0800

    refactor(movie types): Add more movie properties for movie details

--- src/types.ts
+++ src/types.ts
@@ -3,4 +3,10 @@ export type Movie = {
   title: string;
   poster_path: string | null;
   release_date: string;
+  backdrop_path: string | null;
+  popularity: number;
+  overview: string;
+  genre_ids: number[];
+  vote_average: number;
+  vote_count: number;
 };
