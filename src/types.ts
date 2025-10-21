543b18c59e538f8a51dc6f9ddd8a8169e91270dc
Bryan Mangapit <mangapit.bryan@gmail.com>
2025-10-21 10:18:01 +0800

    refactor(movie types): Remove optional property and unused properties

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,5 @@ export type Movie = {
   id: number;
   title: string;
   poster_path: string | null;
-  release_date?: string;
-  overview?: string;
-  vote_average?: number;
+  release_date: string;
 };
